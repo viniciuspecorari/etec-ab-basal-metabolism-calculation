f09eb38f88f6e1de63c85b722c29d8a4ecd46468
vprzo12 <odantewz@gmail.com>
2019-03-24 00:29:47 +0900

    Antes era duas telas agora é uma tela só contendo todo o sistema separados em parte 1 e parte2

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,64 +1,45 @@
-// This is a JavaScript file
-
-//pega o valor dos inputs 
-var id = $('#idade').val();
-var alt = $('#altura').val();
-var pes = $('#peso').val();
-
-$(document).on('click', '#continuar', function(){
-
-//verifica a opção selecionada
-var atiFisica;
-if( atiFisica = $("input[name='seden']:checked").val()){
-
-    //pega o valor do select sexo
-    if($("#sexo option:selected").val() == "masculino"){
-      var sex = "Masculino";
-
-      //faz a conta
-      conta = 1.2 * 66 + [(13,7 * pes) + ( 5 * alt - (6,8 * id))];
-      $('#teste').val(conta);
-      alert("OLAAAAA");
 
 
-    }else{
-      sex="Feminino";
-    }
+// //botão continuar primeira tela
+// $(document).on('click', '#continuar', function(){
 
-}if(atiFisica =$("input[name='levAtivo']:checked").val()){
+// //pega o valor dos inputs 
+// var id = $('#idade').val();
+// var alt = $('#altura').val();
+// var pes = $('#peso').val();
+
+// alert("passouuuu");
+// $('#teste').val(id);
+// alert("passouuuu");
+
+
+// //pega o valor do select sexo
+//  if($("#sexo option:selected").val() == "masculino"){
+
+//     var sex = "Masculino";
+//     alert("passou");
+
+// }else{
+//       sex="Feminino";
+// }
+// });
+
+
+// //verifica a opção selecionada sedentario, ativo etc
+// var atiFisica;
+// if( atiFisica = $("input[name='seden']:checked").val()){
+
+// }if(atiFisica =$("input[name='levAtivo']:checked").val()){
     
-    //pega o valor do select sexo
-    if($("#sexo option:selected").val() == "masculino"){
-      var sex = "Masculino";
-    }else{
-      sex="Feminino";
-    }
-}if(atiFisica = $("input[name='modAtivo']:checked").val()){
+// }if(atiFisica = $("input[name='modAtivo']:checked").val()){
     
-    //pega o valor do select sexo
-    if($("#sexo option:selected").val() == "masculino"){
-      var sex = "Masculino";
-    }else{
-      sex="Feminino";
-    }
-}if(atifisica =$("input[name='altatAtivo']:checked").val()){
+// }if(atifisica =$("input[name='altatAtivo']:checked").val()){
     
-    //pega o valor do select sexo
-    if($("#sexo option:selected").val() == "masculino"){
-      var sex = "Masculino";
-    }else{
-      sex="Feminino";
-    }
-}if(exAtivo =$("input[name='exAtivo']:checked").val()){
+// }if(exAtivo =$("input[name='exAtivo']:checked").val()){
     
-    //pega o valor do select sexo
-    if($("#sexo option:selected").val() == "masculino"){
-      var sex = "Masculino";
-    }else{
-      sex="Feminino";
-    }
-}
-
-});
+// }
 
 
+// //faz a conta homem
+//       conta = 1.2 * 66 + [(13,7 * pes) + ( 5 * alt - (6,8 * id))];
+//       $('#teste').val(conta);
