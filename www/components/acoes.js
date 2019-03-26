7c0abc9d098c406875017f7267e9af8d295b4765
vprzo12 <odantewz@gmail.com>
2019-03-27 02:43:33 +0900

    Correção de alguns problemas - TESTE

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,44 +1,30 @@
 
-// //botão continuar primeira tela
-// $(document).on('click', '#continuar', function(){
+
 
 // //pega o valor dos inputs 
-// var id = $('#idade').val();
-// var alt = $('#altura').val();
-// var pes = $('#peso').val();
-
-// alert("passouuuu");
-// $('#teste').val(id);
-// alert("passouuuu");
+ var id = $('#idade').val();
+ var alt = $('#altura').val();
+ var pes = $('#peso').val();
+ var sex;
 
 
-// //pega o valor do select sexo
-//  if($("#sexo option:selected").val() == "masculino"){
-
-//     var sex = "Masculino";
-//     alert("passou");
-
-// }else{
-//       sex="Feminino";
-// }
-// });
+ //verifica o sexo
+  //codigo
 
 
+
+$(document).on('click', '#calculo', function(){
 // //verifica a opção selecionada sedentario, ativo etc
-// var atiFisica;
-// if( atiFisica = $("input[name='seden']:checked").val()){
+ var atiFisica;
+ var conta;
 
-// }if(atiFisica =$("input[name='levAtivo']:checked").val()){
-    
-// }if(atiFisica = $("input[name='modAtivo']:checked").val()){
-    
-// }if(atifisica =$("input[name='altatAtivo']:checked").val()){
-    
-// }if(exAtivo =$("input[name='exAtivo']:checked").val()){
-    
-// }
+ if( sex = $("input[select='masculino']:checked").val()){
 
-
-// //faz a conta homem
-//       conta = 1.2 * 66 + [(13,7 * pes) + ( 5 * alt - (6,8 * id))];
-//       $('#teste').val(conta);
+    if( sex = $("input[name='seden']:checked").val()){
+   
+ }
+ }
+ if(sex =$("input[select='feminino']:checked").val()){
+    
+ }
+});
